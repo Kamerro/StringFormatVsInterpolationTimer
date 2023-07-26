3da06e21d4ff4ed62bde4297611daff3026c74c1
Kamil Mikuła <kamimik090@student.polsl.pl>
2023-07-26 16:21:05 +0200

    Dodaj pliki projektów.

---
+++ StringFormatVsInterpolationTimer/Program.cs
@@ -0,0 +1,19 @@
+﻿//Declaration of variables:
+string result = String.Empty;
+string OK = "OK";
+string testowy = "testowy";
+string napis = " napis";
+DateTime now;
+DateTime then;
+TimeSpan span;
+
+now = DateTime.Now;
+for (int i = 0; i < 100000000; i++)
+{
+    result = $"{OK}{testowy}{napis}";
+    //result = String.Format(OK, testowy, napis);
+}
+then = DateTime.Now;
+span = then - now;
+Console.WriteLine(span);
+//Conclusion -> String interpolation is a little bit faster than String.Format
