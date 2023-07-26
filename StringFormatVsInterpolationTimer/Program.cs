d988583e2a15c896bf5cd8087fad8e442ef02667
Kamil Mikuła <kamimik090@student.polsl.pl>
2023-07-26 17:01:35 +0200

    Stopwatch with string builder

--- StringFormatVsInterpolationTimer/Program.cs
+++ StringFormatVsInterpolationTimer/Program.cs
@@ -1,17 +1,22 @@
 ﻿//Declaration of variables:
 using System.Diagnostics;
+using System.Text;
 
 string result = String.Empty;
 string OK = "OK";
 string testowy = "testowy";
 string napis = " napis";
+StringBuilder sb = new(); 
 Stopwatch stopwatch = new Stopwatch();
 stopwatch.Start();
-for (int i = 0; i < 100000000; i++)
+for (int i = 0; i < 10000; i++)
 {
-    //result = $"{OK}{testowy}{napis}";
-    result = String.Format(OK, testowy, napis);
+    //result += $"{i}"; //40 ms
+    //result += i.ToString(); //33 ms
+    sb.AppendLine(i.ToString()); // 0.3 ms
+
 }
+sb.ToString();
 stopwatch.Stop();
 Console.WriteLine(stopwatch.Elapsed);
 //Conclusion -> String interpolation is a little bit faster than String.Format
